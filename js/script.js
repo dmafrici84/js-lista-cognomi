34dc9c1f704ff3bd822a26e484f89364d2ce9a5c
Daniele <dmafrici84@gmail.com>
2020-06-24 16:37:21 +0200

    Lc js-lista-cognomi corretto le condizioni per verificare se il numero inserito dall'utente è corretto le ho messe fuori dal for

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
+// creo l'array con i cognomi
 
 var listaCognomi = ["Rossi", "Magenta", "Verdi", "Rosa", "Bianchi", "Marroni"];
 
 console.log(listaCognomi);
+
+// al click del bottone
 
 var btn = document.getElementById("verifica");
 
@@ -9,31 +12,51 @@ btn.addEventListener("click", function () {
 
   var lista = document.getElementById('lista');
 
+  // prendo il valore inserito dall'cognomeUtente
+
   var cognomeUtente = document.getElementById('cognome').value;
-
-  listaCognomi.push(cognomeUtente);
-
-  console.log(listaCognomi);
-
-  listaCognomi = listaCognomi.sort();
-
-  console.log(listaCognomi);
 
   var j = "a";
 
-  for (var i = 0; i < listaCognomi.length; i++) {
+  // pongo delle condizioni affinchè il dato inserito dall'utente non sia corretto
+  
+  if (j > cognomeUtente && isNaN(cognomeUtente) && cognomeUtente != "") {
 
-    if (j > cognomeUtente && isNaN(cognomeUtente) && cognomeUtente != "") {
+    // aggiungo nell'array creato in precedenza il cognome dell'utente
+
+    listaCognomi.push(cognomeUtente);
+
+    console.log(listaCognomi);
+
+    //dispongo l'array in ordine alfabetico
+
+    listaCognomi = listaCognomi.sort();
+
+    console.log(listaCognomi);
+
+    for (var i = 0; i < listaCognomi.length; i++) {
+
+      // stampo i valori dell'array nell html
 
       lista.innerHTML += "<li>" + listaCognomi[i] +"</li>";
 
       console.log(listaCognomi[i]);
 
-    } else {
+      // determino la posizione nella lista del cognome inserito dall'utente
 
-      document.getElementById("avviso").innerHTML = "Attenzione il Cognome inserito non è corretto. Verificare anche che la prima lettera sia maiuscola.";
+      if (cognomeUtente == listaCognomi[i]) {
+
+        document.getElementById("avviso").innerHTML = "Il cognome inserito si trova nella posizione: " + (i+1);
+
+      }
 
     }
+
+  } else {
+
+    // se i dati inseriti dall'utente non sono corretti
+
+    document.getElementById("avviso").innerHTML = "Attenzione il Cognome inserito non è corretto. Verificare anche che la prima lettera sia maiuscola.";
 
   }
 
